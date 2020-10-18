fe708789a26d48521df182332606fcf23e900b40
Pawankumar Chidurala <chpaone09@hotmail.com>
2020-10-19 02:49:13 +0530

    Program to demonstrate TERNARY Operator in java

---
+++ TERNARY.java
@@ -0,0 +1,19 @@
+/*
+***********************************************************
+
+Topic   : Program to demonstrate TERNARY Operator in java.
+Author  : Chpaone09®
+Date    : Oct 19, 2020
+
+************************************************************
+*/
+
+public class TERNARY {
+    public static void main(String[] args) {
+
+        int a = 2, b = 5;
+
+        System.out.print("\n\n\t Bigger Number >> "+ (a > b ? a : b) );
+
+    }
+}
